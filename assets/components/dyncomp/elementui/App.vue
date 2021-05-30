207a02f0574268f5dd12ca6f065d1d247dd4fcac
jacky <xinjie.zhang@gmail.com>
2021-05-30 16:05:18 +0800

    测试：简单动态控件

---
+++ assets/components/dyncomp/elementui/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+  ElementUI
+    <template v-for="block in content.body">
+      <component :is="block.component" :block="block" :key="block._uid"></component>
+    </template>
+  </div>
+</template>
+
+<script>
+import TextInput from "./components/TextInput";
+import SelectInput from "./components/SelectInput";
+
+export default {
+  data() {
+    return {
+      content: {
+        body: [
+          {
+            _uid: "BUY6Drn9e1",
+            component: "TextInput",
+            title: "Foo"
+          },
+          {
+            _uid: "gJZoSLkfZV",
+            component: "select-input",
+            headline: "Bar"
+          },
+          {
+            _uid: "X1JAfdsZxy",
+            component: "text-input",
+            title: " headline"
+          }
+        ]
+      }
+    };
+  },
+  name: "App",
+  components: {
+    TextInput,
+    SelectInput
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ assets/components/dyncomp/simple/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <template v-for="block in content.body">
+      <component :is="block.component" :block="block" :key="block._uid"></component>
+    </template>
+  </div>
+</template>
+
+<script>
+import Bar from "./components/Bar";
+import Foo from "./components/Foo";
+
+export default {
+  data() {
+    return {
+      content: {
+        body: [
+          {
+            _uid: "BUY6Drn9e1",
+            component: "foo",
+            headline: "Foo"
+          },
+          {
+            _uid: "gJZoSLkfZV",
+            component: "bar",
+            title: "Bar"
+          },
+          {
+            _uid: "X1JAfdsZxy",
+            component: "foo",
+            headline: "Another headline"
+          }
+        ]
+      }
+    };
+  },
+  name: "App",
+  components: {
+    Bar,
+    Foo
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
